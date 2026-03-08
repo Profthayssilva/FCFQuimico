8c8c0018c1493076fa7105108e40c4284edee980
Thays Silva <tplsilva05@gmail.com>
2026-03-08 16:37:41 -0300

    remove manifest storage para evitar erro 500

--- FCF/settings.py
+++ FCF/settings.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # ==================================================
